fe2f5daaa2f25a068621bdb7eafa584ad79b5b39
detektyw <kwojno77@gmail.com>
2021-05-07 14:07:28 +0200

    Dodanie ViewBinding, utworzenie prostej aktywności nr 1

--- app/src/main/java/com/example/pastwa_miasta/MainActivity.kt
+++ app/src/main/java/com/example/pastwa_miasta/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.pastwa_miasta
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.pastwa_miasta.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        //Greeting toast
+        Toast.makeText(this, "Witamy w ${binding.gameTitle.text}", Toast.LENGTH_SHORT).show()
+    }
+
+    //button to confirm nickname and run next activity
+    fun confirm(view: View) {
+        if (binding.inputUsername.text.length >= 3)
+            Toast.makeText(this, "Super", Toast.LENGTH_SHORT).show()
     }
 }
