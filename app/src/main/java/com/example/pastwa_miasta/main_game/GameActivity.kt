a348c77ff1511bde95ac7516da5fb464ec6a5a78
Wregan99 <wojtekpakulski@gmail.com>
2021-05-27 20:58:27 +0200

    Nasłuchiwanie bazy w waiting roomie i wyświetlanie kategorii w grze na podstawie danych z bazy

--- app/src/main/java/com/example/pastwa_miasta/main_game/GameActivity.kt
+++ app/src/main/java/com/example/pastwa_miasta/main_game/GameActivity.kt
@@ -9,23 +9,51 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.pastwa_miasta.R
 import com.example.pastwa_miasta.results.ResultsActivity
+import com.google.firebase.database.*
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class GameActivity : AppCompatActivity() {
 
     private lateinit var recyclerView: RecyclerView
     private lateinit var answersList: ArrayList<Answer>
 
+    private var myNick: String? = null
+    private var gameId: String? = null
+
+    private lateinit var db: FirebaseDatabase
+    private lateinit var gameRef: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+        answersList = ArrayList()
+        setViews()
+        db = Firebase.database("https://panstwamiasta-5c811-default-rtdb.europe-west1.firebasedatabase.app/")
 
+        myNick = intent.getStringExtra("myNick")
+        gameId = intent.getStringExtra("gameId").toString()
+        gameRef = db.reference.child("Games").child(gameId!!)
+        getGameCategories()
+    }
+
+    private fun getGameCategories() {
+        gameRef.child("Settings").child("Categories")
+                .addListenerForSingleValueEvent(object : ValueEventListener {
+                    override fun onDataChange(dataSnapshot: DataSnapshot) {
+                        dataSnapshot.children.forEach {
+                            answersList.add(Answer(it.key.toString()))
+                        }
+                        recyclerView.adapter?.notifyDataSetChanged()
+                    }
+
+                    override fun onCancelled(error: DatabaseError) {}
+                })
+    }
+
+    private fun setViews() {
         recyclerView = findViewById(R.id.recyclerViewGame)
         recyclerView.layoutManager = LinearLayoutManager(this)
-        answersList = ArrayList()
-        answersList.add(Answer("Państwa"))
-        answersList.add(Answer("Miasta"))
-        answersList.add(Answer("Rzecz"))
-        answersList.add(Answer("Imię"))
         val customAdapter = InGameAdapter(answersList)
         recyclerView.adapter = customAdapter
         recyclerView.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
--- app/src/main/java/com/example/pastwa_miasta/waiting_room/RoomActivity.kt
+++ app/src/main/java/com/example/pastwa_miasta/waiting_room/RoomActivity.kt
@@ -1,10 +1,9 @@
 package com.example.pastwa_miasta.waiting_room
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -12,22 +11,40 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.pastwa_miasta.Player
 import com.example.pastwa_miasta.R
 import com.example.pastwa_miasta.main_game.GameActivity
+import com.google.firebase.database.*
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class RoomActivity : AppCompatActivity(), IRecyclerViewClick {
-
     private lateinit var recyclerView: RecyclerView
     private lateinit var playersList: ArrayList<Player>
     private lateinit var playerCounterView: TextView
 
+    private var myNick: String? = null
+    private var gameId: String? = null
+
+    private lateinit var db: FirebaseDatabase
+    private lateinit var gameRef: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_room)
+        db = Firebase.database("https://panstwamiasta-5c811-default-rtdb.europe-west1.firebasedatabase.app/")
 
+        myNick = intent.getStringExtra("myNick")
+        //gameId = intent.getStringExtra("gameId").toString()
+        gameId = "1"
+        gameRef = db.reference.child("Games").child(gameId!!)
+        playersList = ArrayList()
+
+        setViews()
+        listenForJoiningPlayers()
+        listenForGameStart()
+    }
+
+    private fun setViews() {
         recyclerView = findViewById(R.id.recyclerViewRoom)
         recyclerView.layoutManager = LinearLayoutManager(this)
-        playersList = ArrayList()
-        playersList.add(Player("Wojtek"))
-        playersList.add(Player("Kacper"))
         val customAdapter = RoomAdapter(playersList, this)
         recyclerView.adapter = customAdapter
         recyclerView.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
@@ -35,12 +52,34 @@ class RoomActivity : AppCompatActivity(), IRecyclerViewClick {
         playerCounterView.text = "${playersList.size} graczy"
     }
 
+    private fun listenForJoiningPlayers() {
+        gameRef.child("Players").addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                playersList.clear()
+                dataSnapshot.children.forEach {
+                    playersList.add(Player(it.key.toString()))
+                }
+                refresh()
+            }
+            override fun onCancelled(error: DatabaseError) {}
+        })
+    }
+
+    private fun listenForGameStart() {
+        gameRef.child("Game_flag").addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                if(dataSnapshot.value == true)
+                    startGame()
+            }
+            override fun onCancelled(error: DatabaseError) {}
+        })
+    }
+
     override fun onItemClick(pos: Int) {
-        Toast.makeText(this, "Kliknięto w gracza ${playersList[pos].name}", Toast.LENGTH_SHORT).show()
-        playersList.add(Player("Wiktoria ${playersList.size}"))
         refresh()
     }
 
+    @SuppressLint("SetTextI18n")
     private fun refresh() {
         playerCounterView.text = "${playersList.size} graczy"
         recyclerView.adapter?.notifyDataSetChanged()
@@ -56,15 +95,18 @@ class RoomActivity : AppCompatActivity(), IRecyclerViewClick {
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
         val restoredAllPictures = savedInstanceState.getParcelableArrayList<Player>("playersList")
-        if (restoredAllPictures != null) {
+        if(restoredAllPictures != null) {
             playersList = restoredAllPictures
             (recyclerView.adapter as RoomAdapter).players = restoredAllPictures
         }
         refresh()
     }
 
-    fun startGame(view: View) {
+    fun startGame() {
         val i = Intent(this, GameActivity::class.java)
+        i.putExtra("myNick", myNick)
+        i.putExtra("gameId", gameId)
         startActivity(i)
+        finish()
     }
 }
