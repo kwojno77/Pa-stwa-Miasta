4a0c8594e8843b5cc679666dcac3e0380ea33f02
admin <kwojno77@gmail.com>
2021-06-06 16:54:03 +0200

    odpowiedzi są zliczane w Answers

--- app/src/main/java/com/example/pastwa_miasta/main_game/GameActivity.kt
+++ app/src/main/java/com/example/pastwa_miasta/main_game/GameActivity.kt
@@ -24,6 +24,7 @@ import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import java.util.*
 import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
 import kotlin.random.Random
 
 
@@ -69,6 +70,7 @@ class GameActivity : AppCompatActivity(), IRecyclerViewClick {
         previousLetter = intent.getStringExtra("previousLetter").toString()
         checkRounds()
         if(onlyResults) {
+            setRepeatedAnswers()
             getPlayersPointsFromDatabase()
             letterView.text = previousLetter
             timerView.visibility = View.INVISIBLE
@@ -86,6 +88,14 @@ class GameActivity : AppCompatActivity(), IRecyclerViewClick {
             getGameCategories()
         }
         timer()
+    }
+
+    private fun setRepeatedAnswers() {
+        /*gameRef.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+            }
+            override fun onCancelled(error: DatabaseError) {}
+        })*/
     }
 
     private fun getResultsFromDatabase() {
@@ -327,6 +337,7 @@ class GameActivity : AppCompatActivity(), IRecyclerViewClick {
                     if(it.key.toString() != "Points") {
                         val map: HashMap<String, String> =
                             (it.value as ArrayList<HashMap<String, String>>).last()
+                        countAnswers(map, dataSnapshot, gameId)
                         for (elem in map) {
                             val isCorrect =
                                 dataSnapshot.child("Keywords").child(it.key!!).child(elem.key.toLowerCase())
@@ -340,6 +351,18 @@ class GameActivity : AppCompatActivity(), IRecyclerViewClick {
             }
             override fun onCancelled(error: DatabaseError) {}
         })
+    }
+
+    private fun countAnswers(map: HashMap<String, String>, dataSnapshot: DataSnapshot, gameId: String) {
+        for (elem in map) {
+            if (dataSnapshot.child(gameId).child("Answers").child(elem.key.toLowerCase()).exists()) {
+                gameRef.child("Answers").child(elem.key.toLowerCase()).setValue(
+                    ServerValue.increment(1L)
+                )
+            } else if (elem.key.toLowerCase().length > 1) {
+                gameRef.child("Answers").child(elem.key.toLowerCase()).setValue(1L)
+            }
+        }
     }
 
     private fun setAnswerTrueOrFalse(category: String, answer: String, isCorrect: Boolean) {
